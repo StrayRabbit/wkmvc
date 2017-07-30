c7bee2812befe6461cce635cd44eb5a8fbce2ded
王辉 <xiaohui_stone@qq.com>
2017-07-30 09:23:40 +0800

    添加RolePermissionManage

---
+++ Service/IService/SysManage/IRolePermissionManage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.IService
+{
+    public interface IRolePermissionManage : IRepository<Domain.SYS_ROLE_PERMISSION>
+    {
+
+    }
+}
